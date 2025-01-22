22afc83f078079f96544a5f42e8b19b391e86912
Natalya Shneyder <nshneyder@ozon.ru>
2025-01-22 18:10:22 +0300

    сделали локальное хранилище

---
+++ Api/Controller/StorageController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class StorageController : BaseController
+{
+    private DataContext dataContext;
+    //через AddSingleton указываем объект, и потом через конструктор класса получаем
+    //доступ к этому объекту из любой части приложения.
+    public StorageController(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+    [HttpGet("SetString/{value}")]
+    public void SetString(string value)
+    {
+        dataContext.Str = value;
+    }
+
+    [HttpGet("GetString")]
+    public string GetString()
+    {
+        return dataContext.Str;
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -4,6 +4,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //добавляет функционал для работы с контроллерами: сервис для обработки http-запросов, сервисы маршрутизации запросов к контроллерам и др.
 builder.Services.AddControllers();
+//внедрение зависимостей, если нужно что-то на уровне всего приложения, то:
+builder.Services.AddSingleton<DataContext>();
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
